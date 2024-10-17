00bdfba193168632f12612b283c4cd8956e62776
Victor Rivas <victorenrique2002@gmail.com>
2024-10-16 22:36:49 -0400

    New functionality to load files into the DataSource from the Welcome interface

--- src/edd_project/EDD_Project.java
+++ src/edd_project/EDD_Project.java
@@ -2,6 +2,7 @@ package edd_project;
 import com.datasource.DataSource;
 import com.graph.Graph;
 import java.io.IOException;
+import com.interfaces.WelcomeInterface;
 
 
 /**
@@ -14,16 +15,18 @@ public class EDD_Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        DataSource dataSource = new DataSource();
-        try {
-//            dataSource.loadNetworkFromFile("src/resources/Bogota.json"); // Carga el archivo de Bogotá
-            // O para Caracas:
-             dataSource.loadNetworkFromFile("src/resources/Caracas.json");
-            System.out.println(dataSource.getNetworkData()); // Muestra los datos cargados
-        } catch (IOException e) {
-            System.err.println("Error loading network data: " + e.getMessage());
-        }
+//        DataSource dataSource = new DataSource();
+//        try {
+////            dataSource.loadNetworkFromFile("src/resources/Bogota.json"); // Carga el archivo de Bogotá
+//            // O para Caracas:
+//             dataSource.loadNetworkFromFile("src/resources/Caracas.json");
+//            System.out.println(dataSource.getNetworkData()); // Muestra los datos cargados
+//        } catch (IOException e) {
+//            System.err.println("Error loading network data: " + e.getMessage());
+//        }
         
+          WelcomeInterface interface1 = new WelcomeInterface();
+          interface1.show();
         
     }
     
